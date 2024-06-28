ea224e7c0d612ab72cfd02f56bba5650f8f4c72c
Joseph A Utuedeye <97887019+utuedey@users.noreply.github.com>
2024-06-28 01:51:54 +0100

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -34,7 +34,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).send('Internal Server Error')
 })
